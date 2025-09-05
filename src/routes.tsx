2f135dc3cd7f3b8b9d1b156037910f63177d285c
NgoTan <phuongtan9920@gmail.com>
2025-09-05 16:32:05 +0700

    feat: sign in and sign up page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,51 +1,55 @@
-import { createBrowserRouter } from "react-router-dom";
-import CatchAllRoute from "@/pages/CatchAllRoute";
-import Error from "@/pages/Error";
-import MainLayout from "@/components/Layouts/MainLayout";
+import { createBrowserRouter } from 'react-router-dom';
+import CatchAllRoute from '@/pages/CatchAllRoute';
+import Error from '@/pages/Error';
+import MainLayout from '@/components/Layouts/MainLayout';
+import { lazy } from 'react';
+
+const SignIn = lazy(() => import('@/pages/AuthSignIn'));
+const SignUp = lazy(() => import('@/pages/AuthSignUp'));
 
 export const ROUTES = {
-  HOME: "/",
-  FORBIDDEN: "/403",
-  NOT_FOUND: "/404",
-  ALL_ROUTE: "*",
+	HOME: '/',
+	FORBIDDEN: '/403',
+	NOT_FOUND: '/404',
+	ALL_ROUTE: '*',
 
-  // Auth
-  SIGN_IN: "/sign-in",
-  SIGN_UP: "/sign-up",
-  FORGOT_PASSWORD: "/forgot-password",
+	// Auth
+	SIGN_IN: '/sign-in',
+	SIGN_UP: '/sign-up',
+	FORGOT_PASSWORD: '/forgot-password',
 } as const;
 
 export const router = createBrowserRouter([
-  {
-    path: ROUTES.HOME,
-    element: <MainLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        index: true,
-        element: <div>Hello</div>,
-      },
-    ],
-  },
-  // Auth
-  {
-    path: ROUTES.SIGN_IN,
-    element: "sign-in",
-    errorElement: <Error />,
-  },
-  {
-    path: ROUTES.SIGN_UP,
-    element: "sign-up",
-    errorElement: <Error />,
-  },
-  {
-    path: ROUTES.FORGOT_PASSWORD,
-    element: "forgot",
-    errorElement: <Error />,
-  },
-  // Common
-  {
-    path: ROUTES.ALL_ROUTE,
-    element: <CatchAllRoute />,
-  },
+	{
+		path: ROUTES.HOME,
+		element: <MainLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <div>Hello</div>,
+			},
+		],
+	},
+	// Auth
+	{
+		path: ROUTES.SIGN_IN,
+		element: <SignIn />,
+		errorElement: <Error />,
+	},
+	{
+		path: ROUTES.SIGN_UP,
+		element: <SignUp />,
+		errorElement: <Error />,
+	},
+	{
+		path: ROUTES.FORGOT_PASSWORD,
+		element: 'forgot',
+		errorElement: <Error />,
+	},
+	// Common
+	{
+		path: ROUTES.ALL_ROUTE,
+		element: <CatchAllRoute />,
+	},
 ]);
